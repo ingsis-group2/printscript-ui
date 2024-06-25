139fa13392f4b9b6a1b1beac8b5826fc568abfee
tomasfreile <freiletomy@gmail.com>
2024-06-24 22:59:54 -0300

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import {defineConfig, loadEnv} from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode}) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
     define: {
@@ -29,6 +29,11 @@ export default defineConfig(({ mode}) => {
           rewrite: (path) => path.replace(/^\/api/, ''),
         }
       }
+    },
+    build: {
+      rollupOptions: {
+        external: ['js-cookie']
+      }
     }
   }
-})
+});
