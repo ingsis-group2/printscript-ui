6fd62f1f0f7dbc3b5d975631d075a3a8c7cc447c
tomasfreile <freiletomy@gmail.com>
2024-06-28 13:46:25 -0300

    update constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
-export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
-export const FRONTEND_URL = import.meta.env.VITE_FRONTEND_URL;
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://snippet-operations:8080";
+export const FRONTEND_URL = import.meta.env.VITE_FRONTEND_URL || "http://localhost:5173";
 export const AUTH0_USERNAME = import.meta.env.VITE_AUTH0_USERNAME as string
 export const AUTH0_PASSWORD = import.meta.env.VITE_AUTH0_PASSWORD as string
 export const AUTH0_DOMAIN = import.meta.env.VITE_AUTH0_DOMAIN || "";
