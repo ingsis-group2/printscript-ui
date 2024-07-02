167b6fc09b230a535a696ed6ded20b8220d7d1d6
tomasfreile <freiletomy@gmail.com>
2024-07-02 11:57:15 -0300

    add correlation id header

--- src/service/axios.ts
+++ src/service/axios.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
+
 const getToken = (): string | null => {
     return localStorage.getItem('accessToken');
 };
@@ -10,6 +12,10 @@ axiosInstance.interceptors.request.use((config) => {
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
+
+    // Add correlation ID header
+    config.headers['X-Correlation-ID'] = uuidv4();  // Generates a new ID for each request
+
     return config;
 }, (error) => {
     return Promise.reject(error);
