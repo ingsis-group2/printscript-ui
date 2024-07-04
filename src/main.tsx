e6392dce1f4b8198672ef78e2d338950d76ce9f2
tomasfreile <freiletomy@gmail.com>
2024-07-04 12:04:57 -0300

    rollback

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,14 @@ import {createRoot} from "react-dom/client";
 import {PaginationProvider} from "./contexts/paginationProvider.tsx";
 import {SnackbarProvider} from "./contexts/snackbarProvider.tsx";
 import {Auth0Provider} from "@auth0/auth0-react";
-import {AUTH0_CLIENT_ID, AUTH0_DOMAIN} from "./utils/constants.ts";
+import {AUTH0_CLIENT_ID} from "./utils/constants.ts";
 
 createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
       <PaginationProvider>
         <SnackbarProvider>
             <Auth0Provider
-                domain={AUTH0_DOMAIN}
+                domain="https://dev-77ie0xuayhhodlbg.us.auth0.com"
                 clientId={AUTH0_CLIENT_ID}
                 authorizationParams={{
                     redirect_uri: window.location.origin,
